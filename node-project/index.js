83f4f98b5d55be212e2c7599936948929012182f
ar-byu <103947632+ar-byu@users.noreply.github.com>
2023-01-06 16:20:51 -0500

    Changed response message

--- node-project/index.js
+++ node-project/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.send('Silas Cooper');
   });
 
 app.listen(3000, function(){
