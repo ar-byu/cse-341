23102a72e545d96173bbdea04cbc6dd37d3ce806
ar-byu <103947632+ar-byu@users.noreply.github.com>
2023-01-14 21:27:05 -0500

    Added week 2 project

--- week-02/index.js
+++ week-02/index.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
 
-const professionalRoutes = require('./routes/professional');
-
+const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(bodyParser.json())
-
-app.use((req, res, next) => {
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   })
+  .use('/', require('./routes'));
 
-app.use('/professional', professionalRoutes);
-
-app.listen(8080)
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
