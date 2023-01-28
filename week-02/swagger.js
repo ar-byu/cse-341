076262e599aae4e5d89eb5eac72c04a93277c6bb
ar-byu <103947632+ar-byu@users.noreply.github.com>
2023-01-27 20:51:05 -0500

    Started API documentation via Swagger

---
+++ week-02/swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const outputFile = './swagger_output.json';
+const endpointsFiles = ['./routes/index.js'];
+
+const doc = {
+    info: {
+      title: 'My Contacts API',
+      description: 'an API and contacts list',
+    },
+    host: 'localhost:8080',
+    schemes: ['http'],
+  };
+
+swaggerAutogen(outputFile, endpointsFiles, doc)
