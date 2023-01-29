80fbacd4d0692fd63655973f9a2413733625ee66
ar-byu <103947632+ar-byu@users.noreply.github.com>
2023-01-28 22:01:24 -0500

    Fixed schemes

--- week-02/swagger.js
+++ week-02/swagger.js
@@ -10,7 +10,7 @@ const doc = {
     },
     // host: 'localhost:8080',
     host: 'cse-341-project.onrender.com',
-    schemes: ['http'],
+    schemes: ['https'],
   };
 
 swaggerAutogen(outputFile, endpointsFiles, doc)
