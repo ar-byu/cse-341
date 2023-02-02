61ff7787b2cd3bcc092004e63ae82487773a9f0c
ar-byu <103947632+ar-byu@users.noreply.github.com>
2023-02-02 16:39:40 -0500

    Troubleshooting

--- project-02/index.js
+++ project-02/index.js
@@ -8,19 +8,19 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app
-    .use('/api-doc', swaggerUi.serve, swaggerUi.setup('swaggerFile'))
-    .use(bodyParser.json())
-    .use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, 2-Key'
-        )
-        res.setHeader('Content-Type', 'application/json')
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        next();
-    })
-    .use('/', require('./routes'));
+  .use('/api-doc', swaggerUi.serve, swaggerUi.setup('swaggerFile'))
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, 2-Key'
+    )
+    res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    next();
+  })
+  .use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
     if (err) {
